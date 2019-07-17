632b5b611f1f3b3b73a9e68050a30c6f1244f82a
Hilson Cantos <HilsonLloyd.Cantos@edu.sait.ca>
2019-07-16 18:21:53 -0600

    First commit. Built project TravelExpertsData including all classes (individual xxxDB.cs not yet created) Datasource from object.

---
+++ ProjectTeam3/TravelExpertsData/TravelExpertsDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsData
+{
+    public static class TravelExpertsDB
+    {
+        public static SqlConnection GetConnection() // this method will be called in the other *DB classes (when connection needs to be made)
+        {
+            string connectionString = @"Data Source=localhost\SAIT; Initial Catalog = TravelExperts; Integrated Security = True";  // @ allows you to put the entire path 
+
+            return new SqlConnection(connectionString);
+        }
+    }
+}
